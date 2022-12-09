199e7c5d61091bf33b494ef49b4de5d996cfbad6
Aitolkun <totita@list.ru>
2022-12-09 16:57:16 +0300

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Айтолкун Анарбекова on 9/12/22.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
